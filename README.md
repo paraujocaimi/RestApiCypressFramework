f1e3582e4d6c1c5580c02452e36a8bacf786c100
Priscila Caimi <paraujocaimi@hotmail.com>
2020-02-21 13:25:25 -0300

    configuração inicial

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-APIAutomatedTest
+API Test Framework 
+
+## Pre requisitos 
+
+- Install Node.js
+- Install npm
+- Install json-server 
+
+
+
+## Setting up the Project 
+
+First of all, do you need to install json-server 
+
+```
+    npm install -g json-server
+```
+
+After that need to create ```db.json``` for initialize the date, like the exemple file. 
+
+Now in file ```cypress.json```, is necessary put the localhost for start the server , and after that use the command line to up the server
+
+To start **JSON server**, use the command line 
+
+```
+    json-server --watch db.json
+```
+
+
+## Runner Test
+
+Inside of the cypress folder, run this command line
+
+```
+    npx cypress open
+```
+
+## Routes
+
+ ### Home
+
+ ```
+   http://localhost:3000
+```
+
+ ### Resources
+
+```
+    http://localhost:3000/characters
+    http://localhost:3000/students
+    http://localhost:3000/staff
+    http://localhost:3000/gryffindor
+```
+
+## References
+
+The Api data was take for https://hp-api.herokuapp.com/ 
+
+Any other exemples can acess the reference page https://github.com/typicode/json-server 
--- cypress/integration/examples/actions.spec.js
+++ cypress/integration/examples/actions.spec.js
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+/// <reference types="Cypress" />
 
 context('Actions', () => {
   beforeEach(() => {
@@ -183,34 +183,17 @@ context('Actions', () => {
   it('.select() - select an option in a <select> element', () => {
     // https://on.cypress.io/select
 
-    // at first, no option should be selected
-    cy.get('.action-select')
-      .should('have.value', '--Select a fruit--')
-
     // Select option(s) with matching text content
     cy.get('.action-select').select('apples')
-    // confirm the apples were selected
-    // note that each value starts with "fr-" in our HTML
-    cy.get('.action-select').should('have.value', 'fr-apples')
 
     cy.get('.action-select-multiple')
-      .select(['apples', 'oranges', 'bananas'])
-      // when getting multiple values, invoke "val" method first
-      .invoke('val')
-      .should('deep.equal', ['fr-apples', 'fr-oranges', 'fr-bananas'])
+    .select(['apples', 'oranges', 'bananas'])
 
     // Select option(s) with matching value
     cy.get('.action-select').select('fr-bananas')
-      // can attach an assertion right away to the element
-      .should('have.value', 'fr-bananas')
 
     cy.get('.action-select-multiple')
       .select(['fr-apples', 'fr-oranges', 'fr-bananas'])
-      .invoke('val')
-      .should('deep.equal', ['fr-apples', 'fr-oranges', 'fr-bananas'])
-    // assert the selected values include oranges
-    cy.get('.action-select-multiple')
-      .invoke('val').should('include', 'fr-oranges')
   })
 
   it('.scrollIntoView() - scroll an element into view', () => {
